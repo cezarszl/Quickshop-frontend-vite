6afed0606c99a1f1c49cbce5f758fdceeb76250e
cezarszl <cezarszlfedora@gmail.com>
2024-10-15 16:13:34 +0200

    feat: add Home component for the main page layout

---
+++ src/home/Home.tsx
@@ -0,0 +1,5 @@
+// import RandomProductsGrid from "@/components/home/RandomProductsGrid";
+import RandomProductsGrid from "@/components/home/RandomProductsGrid";
+export default function Home() {
+  return <div className="main-container">{<RandomProductsGrid />}</div>;
+}
