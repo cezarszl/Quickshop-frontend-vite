0de5e53d40777a81a39e6f09b9233d017e7c57d6
cezarszl <cezarszlfedora@gmail.com>
2024-10-15 18:16:40 +0200

    refactor: rename landing page grid to CategoriesGrid

--- src/components/home/CategoriesGrid/CategoriesGrid.tsx
+++ src/components/home/CategoriesGrid/CategoriesGrid.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo } from "react";
 import { useProductStore, baseUrl } from "@/store/productStore";
-// import { Link } from "react-router-dom";
-const RandomProductsGrid = () => {
+import styles from "./categorysGrid.module.css";
+const CategoriesGrid = () => {
   const { randomProducts, fetchRandomProducts, minPrices, fetchMinPrices } =
     useProductStore();
 
@@ -18,28 +18,24 @@ const RandomProductsGrid = () => {
   }, [minPrices]);
 
   return (
-    <div className="products-grid">
-      {" "}
+    <div className={styles.gridContainer}>
       {randomProducts.map((product) => (
-        <div key={product.id} className="product-card">
-          {" "}
+        <div key={product.id} className={styles.productCard}>
           {/* <Link to={`/product/${product.id}`}> */}
-          <div className="product-image-container">
-            {" "}
+          <div className={styles.productImageContainer}>
             <img
+              className={styles.productImage}
               src={`${baseUrl}${product.imageUrl}`}
               alt={product.name}
-              className="product-image"
             />
-            <div className="product-info-overlay">
-              {" "}
-              <h1 className="product-category-name">{product.categoryName}</h1>
+            <div className={styles.productInfo}>
+              <h1 className={styles.productCategory}>{product.categoryName}</h1>
               {minPricesMap[product.categoryId] !== undefined ? (
-                <p className="product-price">
+                <p className={styles.productPrice}>
                   From ${minPricesMap[product.categoryId].toFixed(2)}
                 </p>
               ) : (
-                <p className="product-price">Price not available</p>
+                <p className={styles.productPrice}>Price not available</p>
               )}
             </div>
           </div>
@@ -50,4 +46,4 @@ const RandomProductsGrid = () => {
   );
 };
 
-export default RandomProductsGrid;
+export default CategoriesGrid;
