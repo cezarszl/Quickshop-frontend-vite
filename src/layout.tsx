ba1c0f5369cdf39fd4f187a10d3c345c9edb564a
cezarszl <cezarszlfedora@gmail.com>
2024-10-15 19:05:49 +0200

    refactor: add footer and reorganised layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Sidebar from "@/components/sidebar/SideBar";
 import styles from "./layout.module.css";
+import Footer from "./components/footer/Footer";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className={styles.layoutContainer}>
-      <Sidebar />
-      <main className={styles.mainContent}>{children}</main>
+    <div className="layout">
+      <div className={styles.mainContainer}>
+        <Sidebar />
+        <main className={styles.contentArea}>{children}</main>
+      </div>
+      <Footer />
     </div>
   );
 };
