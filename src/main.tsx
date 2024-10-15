f0d63fba060d73a8271f3644f380f34c201dc778
cezarszl <cezarszlfedora@gmail.com>
2024-10-15 18:15:45 +0200

    feat: add BrowserRouter and its routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import Home from "./home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>
-);
+const App = () => {
+  return (
+    <StrictMode>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </StrictMode>
+  );
+};
+createRoot(document.getElementById("root")!).render(<App />);
