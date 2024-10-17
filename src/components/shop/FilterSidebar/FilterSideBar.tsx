b843edfa5c51013bcbb2d0398ba418fcc7efb5f9
cezarszl <cezarszlfedora@gmail.com>
2024-10-17 23:11:24 +0200

    fix: typo

--- src/components/shop/FilterSidebar/FilterSideBar.tsx
+++ src/components/shop/FilterSidebar/FilterSideBar.tsx
@@ -1,4 +1,4 @@
-import styles from "./filterSidebar.module.css";
+import styles from "./filterSideBar.module.css";
 
 export default function FilterSidebar() {
   return <div className={styles.filterSideBarContainer}>FilterSidebar</div>;
