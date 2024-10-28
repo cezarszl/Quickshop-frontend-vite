4f4746fddaf72721a348f0d607dd9c6e9429bc32
cezarszl <cezarszlfedora@gmail.com>
2024-10-28 19:31:20 +0100

    feat: display categories in FilterSidebar

--- src/components/shop/FilterSideBar/FilterSideBar.tsx
+++ src/components/shop/FilterSideBar/FilterSideBar.tsx
@@ -1,7 +1,29 @@
+import { useEffect } from "react";
 import styles from "./filterSideBar.module.css";
+import { useCategoryStore } from "@/store/categoryStore";
 
 const FilterSidebar: React.FC = () => {
-  return <div className={styles.filterSideBarContainer}></div>;
+  const { categories, fetchCategories } = useCategoryStore();
+
+  useEffect(() => {
+    fetchCategories();
+  }, [fetchCategories]);
+
+  console.log(categories);
+  return (
+    <div className={styles.filterSideBarContainer}>
+      <div className={styles.filterSideBar}>
+        <h2 className={styles.title}>Categories</h2>
+        <ul className={styles.categoryList}>
+          {categories.map((category) => (
+            <li key={category.id} className={styles.categoryItem}>
+              {category.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default FilterSidebar;
