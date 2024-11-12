2d7d478953555685bb8602557cce3ac9e8bfc425
cezarszl <cezarszlfedora@gmail.com>
2024-11-12 17:17:08 +0100

    feat: add products to shop

--- src/shop/Shop.tsx
+++ src/shop/Shop.tsx
@@ -1,14 +1,29 @@
 import FilterSideBar from "@/components/shop/FilterSideBar/FilterSideBar";
 import styles from "./shop.module.css";
+import { useProductStore } from "@/store/productStore";
+import ProductCard from "@/components/shop/ProducCard/ProductCard";
+import { useEffect } from "react";
 
 const Shop: React.FC = () => {
+  const { products, fetchProducts } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log(products);
+
   return (
     <div className={styles.shopContainer}>
       <div className={styles.filterSideBar}>
         <FilterSideBar />
       </div>
       <div className={styles.shopContent}>
-        {/* Tutaj wstawiasz treść sklepu */}
+        <div className={styles.productsGrid}>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
