a67e23137c9da64979cee14f10a33c70f2d774cf
cezarszl <cezarszlfedora@gmail.com>
2024-11-16 23:38:03 +0100

    feat: add top bar component to shop page

--- src/shop/Shop.tsx
+++ src/shop/Shop.tsx
@@ -3,13 +3,14 @@ import styles from "./shop.module.css";
 import { useProductStore } from "@/store/productStore";
 import ProductCard from "@/components/shop/ProducCard/ProductCard";
 import { useEffect } from "react";
+import TopBar from "@/components/shop/TopBar/TopBar";
 
 const Shop: React.FC = () => {
-  const { products, fetchProducts } = useProductStore();
-
+  // const { products, fetchProducts } = useProductStore();
+  const { products, fetchFilteredProducts } = useProductStore();
   useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
+    fetchFilteredProducts();
+  }, [fetchFilteredProducts]);
 
   return (
     <div className={styles.shopContainer}>
@@ -17,6 +18,7 @@ const Shop: React.FC = () => {
         <FilterSideBar />
       </div>
       <div className={styles.shopContent}>
+        <TopBar />
         <div className={styles.productsGrid}>
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
