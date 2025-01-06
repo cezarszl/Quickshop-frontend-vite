39616c268537fbdc45ebee8e198844973dd6be92
cezarszl <cezarszlfedora@gmail.com>
2025-01-06 23:08:17 +0100

    feat: add Cart route and component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../home/Home";
-import Shop from "../shop/Shop";
+import Home from "@/home/Home";
+import Shop from "@/shop/Shop";
+import Cart from "@/cart/Cart";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
   );
 };
