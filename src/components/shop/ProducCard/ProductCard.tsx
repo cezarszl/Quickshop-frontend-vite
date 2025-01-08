ff48f2e21d15811d986e06a3ae397bd6ee3b5d9f
cezarszl <cezarszlfedora@gmail.com>
2025-01-09 00:42:20 +0100

    feat: add cart functionality to ProductCard component

--- src/components/shop/ProducCard/ProductCard.tsx
+++ src/components/shop/ProducCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/store/productStore";
 import styles from "./productCard.module.css";
 import axiosInstance from "@/helpers/axiosInstance";
+import { useCartStore } from "@/store/cartStore";
 
 interface ProductCardProps {
   product: Product;
@@ -9,6 +10,15 @@ interface ProductCardProps {
 const baseUrl = axiosInstance.defaults.baseURL;
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const addToCart = useCartStore((state) => state.addToCart);
+
+  const handleAddToCart = async () => {
+    try {
+      await addToCart(product.id, 1);
+    } catch (error) {
+      console.error("Failed to add to cart:", error);
+    }
+  };
   return (
     <div className={styles.card}>
       <img
@@ -24,7 +34,9 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           <h6 className={styles.name}>{product.name}</h6>
         </div>
         <div className={styles.basketWrapper}>
-          <div className={styles.basket}>🛒</div>
+          <div className={styles.basket} onClick={handleAddToCart}>
+            🛒
+          </div>
           <span className={styles.tooltip}>Add to Cart</span>
         </div>
       </div>
