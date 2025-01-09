2b110762345f2a3f6703ada589070a5f368eac20
cezarszl <cezarszlfedora@gmail.com>
2025-01-09 23:38:38 +0100

    feat: enhance FilterSidebar with active category state and improved styling

--- src/components/shop/FilterSideBar/FilterSideBar.tsx
+++ src/components/shop/FilterSideBar/FilterSideBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./filterSideBar.module.css";
 import { useCategoryStore } from "@/store/categoryStore";
 import { useBrandStore } from "@/store/brandStore";
@@ -12,6 +12,7 @@ const FilterSidebar: React.FC = () => {
   const { colors, fetchColors } = useColorStore();
   const { filters, setFilter, fetchFilteredProducts, toogleBrand } =
     useProductStore();
+  const [activeCategory, setActiveCategory] = useState(null);
 
   useEffect(() => {
     fetchCategories();
@@ -21,6 +22,7 @@ const FilterSidebar: React.FC = () => {
 
   const handleCategoryClick = (categoryId: number) => {
     setFilter("categoryId", categoryId);
+    setActiveCategory(categoryId);
     fetchFilteredProducts();
   };
 
@@ -35,85 +37,85 @@ const FilterSidebar: React.FC = () => {
   };
 
   return (
-    <div className={styles.filterSideBarContainer}>
-      <div className={styles.filterSideBar}>
-        {/* Categories Section */}
-        <div className={styles.categoriesSection}>
-          <h3 className={styles.title}>Categories</h3>
-          <ul
-            className={styles.categoryList}
-            role="group"
-            aria-labelledby="category-selection"
-          >
-            {categories.map((category) => (
-              <li
-                key={category.id}
-                className={styles.categoryItem}
-                onClick={() => handleCategoryClick(category.id)}
-              >
-                {category.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* Brands Section */}
-        <div className={styles.brandsSection}>
-          <h3 id="brand-selection" className={styles.subtitle}>
-            Brands
-          </h3>
-          <ul
-            className={styles.brandsList}
-            role="group"
-            aria-labelledby="brand-selection"
-          >
-            {brands.map((brand) => (
-              <li key={brand.id} className={styles.brandsItem}>
-                <label
-                  htmlFor={`brand-${brand.id}`}
-                  className={styles.brandLabel}
-                >
-                  <input
-                    type="checkbox"
-                    id={`brand-${brand.id}`}
-                    value={brand.id}
-                    className={styles.checkbox}
-                    checked={filters.brandIds?.includes(brand.id)}
-                    onChange={() => handleBrandChange(brand.id)}
-                  />
-                  {brand.name}
-                </label>
-              </li>
-            ))}
-          </ul>
-        </div>
-        {/* Colors Section */}
-        <div className={styles.colorsSection}>
-          <h3 className={styles.title}>Color</h3>
-          <ul
-            className={styles.colorsList}
-            role="group"
-            aria-labelledby="colors-selection"
-          >
-            {colors.map((color) => (
-              <li key={color.id} className={styles.colorsItem}>
-                <span
-                  className={styles.colorCircle}
-                  style={{ backgroundColor: color.name.toLocaleUpperCase() }}
-                  title={color.name}
-                  onClick={() => handleColorClick(color.id)}
-                ></span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        {/* Price Slider Section */}
-        <div className={styles.priceSliderSection}>
-          <h3 className={styles.title}>Price</h3>
-          <PriceSlider />
-        </div>
+    <>
+      {/* Categories Section */}
+      <div className={styles.categoriesSection}>
+        <h3 className={styles.categoriesSectionTitle}>Categories</h3>
+        <ul
+          className={styles.categoriesList}
+          role="group"
+          aria-labelledby="category-selection"
+        >
+          {categories.map((category) => (
+            <li
+              key={category.id}
+              className={`${styles.categoriesItem} ${
+                activeCategory === category.id ? styles.activeCategory : ""
+              }`}
+              onClick={() => handleCategoryClick(category.id)}
+            >
+              {category.name}
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
+
+      {/* Brands Section */}
+      <div className={styles.brandsSection}>
+        <h3 id="brand-selection" className={styles.brandsSectionTitle}>
+          Brands
+        </h3>
+        <ul
+          className={styles.brandsList}
+          role="group"
+          aria-labelledby="brand-selection"
+        >
+          {brands.map((brand) => (
+            <li key={brand.id} className={styles.brandsItem}>
+              <input
+                type="checkbox"
+                id={`brand-${brand.id}`}
+                value={brand.id}
+                className={styles.checkbox}
+                checked={filters.brandIds?.includes(brand.id)}
+                onChange={() => handleBrandChange(brand.id)}
+              />
+              <label
+                htmlFor={`brand-${brand.id}`}
+                className={styles.brandLabel}
+              >
+                {brand.name}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* Colors Section */}
+      <div className={styles.colorsSection}>
+        <h3 className={styles.colorsSectionTitle}>Color</h3>
+        <ul
+          className={styles.colorsList}
+          role="group"
+          aria-labelledby="colors-selection"
+        >
+          {colors.map((color) => (
+            <li key={color.id} className={styles.colorsItem}>
+              <span
+                className={styles.colorCircle}
+                style={{ backgroundColor: color.name.toLocaleUpperCase() }}
+                title={color.name}
+                onClick={() => handleColorClick(color.id)}
+              ></span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* Price Slider Section */}
+      <div className={styles.priceSliderSection}>
+        <h3 className={styles.priceSliderSectionTitle}>Price</h3>
+        <PriceSlider />
+      </div>
+    </>
   );
 };
 
