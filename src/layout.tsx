078d4f93017ed2cf775699acaec9826221b32dfc
cezarszl <cezarszlfedora@gmail.com>
2025-01-16 00:59:35 +0100

    style: add Header component to layout for improved structure

--- src/layout.tsx
+++ src/layout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Sidebar from "@/components/sidebar/SideBar";
 import styles from "./layout.module.css";
 import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
+      <Header />
       <div className={styles.mainContainer}>
         <Sidebar />
         {children}
