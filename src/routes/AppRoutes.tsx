089692ef2b8e7e74142ad9261c1a24c78ca7b81e
cezarszl <cezarszlfedora@gmail.com>
2025-01-17 21:43:04 +0100

    fix: update import paths for Home, Shop, and Cart components

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@/home/Home";
-import Shop from "@/shop/Shop";
-import Cart from "@/cart/Cart";
+import Home from "@/pages/home/Home";
+import Shop from "@/pages/shop/Shop";
+import Cart from "@/pages/cart/Cart";
 
 const AppRoutes: React.FC = () => {
   return (
