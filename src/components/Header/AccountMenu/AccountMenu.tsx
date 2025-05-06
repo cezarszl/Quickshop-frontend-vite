e02f27d2397e4cfd50cb7dce155171468fdfda6c
cezarszl <cezarszlfedora@gmail.com>
2025-05-06 17:04:03 +0200

    feat: implement logout functionality in AccountMenu component

--- src/components/Header/AccountMenu/AccountMenu.tsx
+++ src/components/Header/AccountMenu/AccountMenu.tsx
@@ -1,17 +1,32 @@
+import { useLoginStore } from "@/stores/loginStore";
 import styles from "./account-menu.module.css";
+import { useNavigate } from "react-router-dom";
 
 interface AccountMenuProps {}
 
 const AccountMenu: React.FC<AccountMenuProps> = () => {
+  const isLoggedIn = useLoginStore((state) => state.isLoggedIn);
+  const logout = useLoginStore((state) => state.logout);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
   return (
     <div className={styles.accountMenu}>
       <ul>
         <li>My profile</li>
         <li>My orders</li>
       </ul>
-      <a href="/login" className={styles.loginBtn}>
-        LOGIN
-      </a>
+      {isLoggedIn ? (
+        <button onClick={handleLogout}>LOGOUT</button>
+      ) : (
+        <a href="/login" className={styles.loginBtn}>
+          LOGIN
+        </a>
+      )}
       <a href="/register" className={styles.registerLink}>
         New here? Register now
       </a>
