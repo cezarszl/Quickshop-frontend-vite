d3e610cbb1d7144c4889ebcfaf3a48a6f5a0ad05
cezarszl <cezarszlfedora@gmail.com>
2025-05-12 13:13:38 +0200

    refactor: restructure order display in MyOrders component for improved readability

--- src/pages/myorders/MyOrders.tsx
+++ src/pages/myorders/MyOrders.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useOrderStore } from "@/stores/orderStore";
 import { useLoginStore } from "@/stores/loginStore";
 import styles from "./myOrders.module.css";
+import { Link } from "react-router-dom";
 
 const MyOrders: React.FC = () => {
   const { orders, fetchUserOrders } = useOrderStore();
@@ -30,19 +31,36 @@ const MyOrders: React.FC = () => {
       <h2 className={styles.title}>My Orders</h2>
       {orders.map((order) => (
         <div key={order.id} className={styles.orderCard}>
-          <p>
-            <strong>Order ID:</strong> {order.id}
-          </p>
-          <p>
-            <strong>Date:</strong> {new Date(order.createdAt).toLocaleString()}
-          </p>
-          <p>
-            <strong>Total:</strong> ${order.totalAmount.toFixed(2)}
-          </p>
-          <ul>
+          <div className={styles.orderHeader}>
+            <p>
+              <strong>Order ID:</strong> {order.id}
+            </p>
+            <p>
+              <strong>Date:</strong>{" "}
+              {new Date(order.createdAt).toLocaleString()}
+            </p>
+            <p>
+              <strong>Total:</strong> ${order.totalAmount.toFixed(2)}
+            </p>
+          </div>
+
+          <ul className={styles.productList}>
             {order.items.map((item, index) => (
-              <li key={index}>
-                Product ID: {item.productId}, Quantity: {item.quantity}
+              <li key={index} className={styles.productItem}>
+                <div className={styles.productDetails}>
+                  <img
+                    src={item.productDetails?.imageUrl}
+                    alt={item.productDetails?.name}
+                    className={styles.productImage}
+                  />
+                  <div>
+                    <Link to={`/product/${item.productDetails?.id}`}>
+                      <h4>{item.productDetails?.name}</h4>
+                    </Link>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Price: ${item.productDetails?.price.toFixed(2)}</p>
+                  </div>
+                </div>
               </li>
             ))}
           </ul>
