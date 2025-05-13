b30279b9c84678c3e8f278d39592bf35b6d02275
cezarszl <cezarszlfedora@gmail.com>
2025-05-13 18:46:05 +0200

    feat: update CategoriesGrid layout and improve accessibility with lazy loading for images

--- src/components/Home/CategoriesGrid/CategoriesGrid.tsx
+++ src/components/Home/CategoriesGrid/CategoriesGrid.tsx
@@ -18,35 +18,38 @@ const CategoriesGrid: React.FC = () => {
   }, {} as Record<number, number>);
 
   return (
-    <div className={styles.gridContainer}>
-      {randomProducts.map((product) => (
-        <Link
-          key={product.id}
-          to={`/category/${product.categoryName.toLowerCase()}`}
-        >
-          <div className={styles.productCard}>
-            <div className={styles.productImageContainer}>
-              <img
-                className={styles.productImage}
-                src={product.imageUrl}
-                alt={product.name}
-              />
-              <div className={styles.productInfo}>
-                <h1 className={styles.productCategory}>
-                  {product.categoryName}
-                </h1>
-                {minPricesMap[product.categoryId] !== undefined ? (
-                  <p className={styles.productPrice}>
-                    From ${minPricesMap[product.categoryId].toFixed(2)}
-                  </p>
-                ) : (
-                  <p className={styles.productPrice}>Price not available</p>
-                )}
+    <div className={styles.pageContainer}>
+      <div className={styles.gridContainer}>
+        {randomProducts.map((product) => (
+          <Link
+            key={product.id}
+            to={`/category/${product.categoryName.toLowerCase()}`}
+          >
+            <div className={styles.productCard}>
+              <div className={styles.productImageContainer}>
+                <img
+                  className={styles.productImage}
+                  src={product.imageUrl}
+                  alt={product.categoryName}
+                  loading="lazy"
+                />
+                <div className={styles.productInfo}>
+                  <h2 className={styles.productCategory}>
+                    {product.categoryName}
+                  </h2>
+                  {minPricesMap[product.categoryId] !== undefined ? (
+                    <p className={styles.productPrice}>
+                      From ${minPricesMap[product.categoryId].toFixed(2)}
+                    </p>
+                  ) : (
+                    <p className={styles.productPrice}>Price not available</p>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
